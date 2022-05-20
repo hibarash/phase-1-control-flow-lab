c7192a4566845f815e261cc1a9fc674990d12cb0
hiba <hibzrashed@gmail.com>
2022-05-20 16:43:12 -0500

    commit tree 2aaa97645b73d6d8eb93dcd68aeadde791cec5d2

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+  //const distance = scuberGreetingForFeet;
+//let scuberGreetingForFeet;
+
+  
+  if (distanceInFeet <= 400) {
+    return 'This one is on me!'
+  } 
+  else if ((distanceInFeet > 400 && distanceInFeet < 2000)) {
+    return 'That will be twenty bucks.'
+  }
+  else if (distanceInFeet > 2000 && distanceInFeet < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+
+
+
+function ternaryCheckCity(isDestinationCity){
   // Write your code here!
+  return isDestinationCity === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+ 
+
+switch (tip) {
+  case 'generous':
+    return 'Thank you so much.'
+  case 'not as generous':
+    return 'Thank you.'
+  case 'thanks for everything':
+    return 'Bye.'
+    
+}
 }
